f2bcfe629519564e3c96c0c785073d1de2fdfaeb
Ryan Rice <ryanr1230@gmail.com>
2014-10-24 22:52:18 -0500

    actually working and only somewhat unreadable

--- popup.js
+++ popup.js
@@ -3,11 +3,10 @@ window.onload = function () {
   document.getElementById('button').onclick = function () {
     var username = document.getElementById('username').value;
     var password = document.getElementById('password').value;
-    teamsLogin(username,password, function(req,cookie) { 
+    teamsLogin(username,password, function(req) { 
 	response = new XMLHttpRequest();
 	response.open('GET','https://my-teams.austinisd.org/selfserve/PSSViewReportCardsAction.do',false);
 	response.withCredentials = true;
-	response.setRequestHeader('Set-Cookie',cookie);
 	response.send(null);
 	document.getElementById("response").innerHTML = response.responseText;
     });
--- teamsLogin.js
+++ teamsLogin.js
@@ -1,29 +1,41 @@
 var teamsLogin = function(username, password, callback) { 
     //this should be a promise
-    var req = new XMLHttpRequest();
-    req.open('POST', 'https://my.austinisd.org/WebNetworkAuth/', false);
-    req.setRequestHeader('Accept','*/*');
-    req.setRequestHeader('Content-Type','application/x-www-form-urlencoded')
+    var myreq = new XMLHttpRequest();
+    myreq.open('POST', 'https://my.austinisd.org/WebNetworkAuth/', false);
+
+    myreq.setRequestHeader('Accept','*/*');
     
-    req.send('cn=' + username + '&%5Bpassword%5D=' + password);
-    var headers = req.getAllResponseHeaders().toLowerCase();
+    myreq.setRequestHeader('Content-Type','application/x-www-form-urlencoded')
+    myreq.send('cn=' + username + '&%5Bpassword%5D=' + password);
+    console.log(myreq);
     chrome.cookies.getAll({ name:'CStoneSessionID'}, function(mycookie) { 
-        console.log(mycookie[0]);
-        req.open('GET','https://my-teams.austinisd.org/selfserve/EntryPointSignOnAction.do?parent=false',false);
-        req.withCredentials = true;
-        req.setRequestHeader('Set-Cookie','CStoneSessionID='+mycookie[0].value);
-        req.send('');
-        chrome.cookies.getAll({domain:'my-teamsselfserve.austinisd.org', name:'JSESSIONID'}, function(teamscookie){
-          console.log(teamscookie);
-          req.open('POST','https://my-teams.austinisd.org/selfserve/EntryPointSignOnAction.do?parent=false', false);
-          var reqcookie = 'CStoneSessionID='+mycookie[0].value+'; ';  
-          for(var i = 0; i < teamscookie.length; i++) {
-            reqcookie+= 'JSESSIONID='+teamscookie[i].value+'; ';
-          }
-          console.log(reqcookie);
-          req.setRequestHeader('Set-Cookie',reqcookie);
-          req.send("userLoginId=" + username + "&userPassword=" + password);
-          callback(req,reqcookie);
+        var teams_cookie_req = new XMLHttpRequest();
+        teams_cookie_req.open('POST','https://my-teams.austinisd.org/selfserve/EntryPointSignOnAction.do?parent=false',false);
+        teams_cookie_req.withCredentials = true;
+        teams_cookie_req.setRequestHeader('Accept','*/*');
+
+        teams_cookie_req.setRequestHeader('Content-Type','application/x-www-form-urlencoded')
+        chrome.cookies.set({url:'https://.austinisd.org',name:'CStoneSessionID',value:mycookie[0].value}, function(set_cstone_cookie) { 
+          teams_cookie_req.send(null);
+	  console.log(teams_cookie_req);
+          chrome.cookies.getAll({domain:'my-teams.austinisd.org', name:'JSESSIONID'}, function(teamscookies){
+            var teams_req = new XMLHttpRequest();
+            teams_req.open('POST','https://my-teams.austinisd.org/selfserve/SignOnLoginAction.do', false);
+            var i = teamscookies.length;
+            teamscookies.forEach(function(teamscookie) {
+              chrome.cookies.set({url:'https://my-teams.austinisd.org', name: 'JSESSIONID', value:teamscookie.value}, function(set_teams_cookie) { 
+                i--;
+                if(i === 0) { 
+                  teams_req.setRequestHeader('Accept','*/*');
+                  teams_req.setRequestHeader('Content-Type','application/x-www-form-urlencoded')
+                  teams_req.withCredentials=true;
+                  teams_req.send("userLoginId=" + username + "&userPassword=" + password);
+		  console.log(teams_req);
+                  callback(teams_req);
+                }
+	      }); 
+            });
+          });
         });
     });
 }
