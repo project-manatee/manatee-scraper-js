19494ef5c823c1756bb15f9b189bc6d7fe9c9f35
Sam G <thegladiusmaximus@gmail.com>
2015-02-05 09:39:20 -0600

    added timeout

--- ManaTEAMS/ManaTEAMS.js
+++ ManaTEAMS/ManaTEAMS.js
@@ -12,7 +12,7 @@ function ManaTEAMS(username, password) {
     this.isLoggedIn = false;
 }
 
-ManaTEAMS.prototype.login = function(callback) {
+ManaTEAMS.prototype.login = function(success, error) {
     this.isLoggedIn = true;
     var username = this.username;
     var password = this.password;
@@ -21,16 +21,15 @@ ManaTEAMS.prototype.login = function(callback) {
     //this should be a promise
     var myreq = new XMLHttpRequest();
     myreq.open('POST', 'https://my.austinisd.org/WebNetworkAuth/', false);
-
     myreq.setRequestHeader('Accept', '*/*');
-
     myreq.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
     myreq.send('cn=' + username + '&%5Bpassword%5D=' + password);
     chrome.cookies.getAll({
         name: 'CStoneSessionID'
     }, function(mycookie) {
         var teams_cookie_req = new XMLHttpRequest();
-        teams_cookie_req.open('POST', teamsHost + '/selfserve/EntryPointSignOnAction.do?parent=' + isParent, false);
+        teams_cookie_req.open('POST', teamsHost + '/selfserve/EntryPointSignOnAction.do?parent=' + isParent, true);
+		teams_cookie_req.timeout = 5000; // TODO: tune this
         teams_cookie_req.withCredentials = true;
         teams_cookie_req.setRequestHeader('Accept', '*/*');
         teams_cookie_req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
@@ -39,44 +38,58 @@ ManaTEAMS.prototype.login = function(callback) {
             name: 'CStoneSessionID',
             value: mycookie[0].value
         }, function(set_cstone_cookie) {
+			teams_cookie_req.onload = function(e) {
+				if (teams_cookie_req.readyState === 4) {
+					if (teams_cookie_req.status === 200) {
+						chrome.cookies.getAll({
+							domain: 'my-teams.austinisd.org',
+							name: 'JSESSIONID'
+						}, function(teamscookies) {
+							var teams_req = new XMLHttpRequest();
+							teams_req.open('POST', teamsHost + '/selfserve/SignOnLoginAction.do', false);
+							var i = teamscookies.length;
+							teamscookies.forEach(function(teamscookie) {
+								chrome.cookies.set({
+									url: teamsHost,
+									name: 'JSESSIONID',
+									value: teamscookie.value
+								}, function(set_teams_cookie) {
+									// Crappy fake async foreach
+									i--;
+									if (i === 0) {
+										teams_req.setRequestHeader('Accept', '*/*');
+										teams_req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
+										teams_req.withCredentials = true;
+										teams_req.send("userLoginId=" + username + "&userPassword=" + password);
+										if (isParent) {
+											var studentInfoLocID = TEAMSParser.parseStudentInfoLocID(response);
+											//TODO Hardcoded user index 0 for now
+											var student_choice_request = new XMLHttpRequest();
+											student_choice_request.open('POST', teamsHost + "/selfserve/ViewStudentListChangeTabDisplayAction.do", false);
+											student_choice_request.setRequestHeader('Accept', '*/*');
+											student_choice_request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
+											student_choice_request.withCredentials = true;
+											student_choice_request.send("selectedIndexId=0&studentLocId=" + studentInfoLocID + "&selectedTable=table");
+											success("&selectedIndexId=0&studentLocId=" + studentInfoLocID + "&selectedTable=table", 'success');
+										} else {
+											success(null, 'success');
+										}
+									}
+								});
+							});
+						});
+					} else {
+						error('wrong credentials');
+					}
+				}
+			};
+			teams_cookie_req.onerror = function(e) {
+				error('wrong credentials')
+			};
+			teams_cookie_req.ontimeout = function() {
+				error('timeout')
+			};
             teams_cookie_req.send(null);
-            chrome.cookies.getAll({
-                domain: 'my-teams.austinisd.org',
-                name: 'JSESSIONID'
-            }, function(teamscookies) {
-                var teams_req = new XMLHttpRequest();
-                teams_req.open('POST', teamsHost + '/selfserve/SignOnLoginAction.do', false);
-                var i = teamscookies.length;
-                teamscookies.forEach(function(teamscookie) {
-                    chrome.cookies.set({
-                        url: teamsHost,
-                        name: 'JSESSIONID',
-                        value: teamscookie.value
-                    }, function(set_teams_cookie) {
-                        // Crappy fake async foreach
-                        i--;
-                        if (i === 0) {
-                            teams_req.setRequestHeader('Accept', '*/*');
-                            teams_req.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded')
-                            teams_req.withCredentials = true;
-                            teams_req.send("userLoginId=" + username + "&userPassword=" + password);
-                            if (isParent) {
-                                var studentInfoLocID = TEAMSParser.parseStudentInfoLocID(response);
-                                //TODO Hardcoded user index 0 for now
-                                var student_choice_request = new XMLHttpRequest();
-                                student_choice_request.open('POST', teamsHost + "/selfserve/ViewStudentListChangeTabDisplayAction.do", false);
-                                student_choice_request.setRequestHeader('Accept', '*/*');
-                                student_choice_request.setRequestHeader('Content-Type', 'application/x-www-form-urlencoded');
-                                student_choice_request.withCredentials = true;
-                                student_choice_request.send("selectedIndexId=0&studentLocId=" + studentInfoLocID + "&selectedTable=table");
-                                callback("&selectedIndexId=0&studentLocId=" + studentInfoLocID + "&selectedTable=table");
-                            } else {
-                                callback(null);
-                            }
-                        }
-                    });
-                });
-            });
         });
     });
 }
